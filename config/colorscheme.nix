4b1ac29ffb094f36589d6c991e0e83ddc1502b2a
Ben Homan <ben@benhoman.com>
2024-03-17 23:05:31 -0400

    catppuccin

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,10 +1,22 @@
 {
   colorschemes = {
-    tokyonight = {
+    catppuccin = {
       enable = true;
+      flavour = "mocha";
+      transparentBackground = true;
+      integrations = {
+        fidget = true;
+        gitsigns = true;
+        harpoon = true;
+        treesitter = true;
+        treesitter_context = true;
+        which_key = true;
+      };
+    };
+    tokyonight = {
+      enable = false;
       style = "night";
       transparent = true;
     };
   };
 }
-
